1c970dd369511d91c6948c0f073d208d958c16d4
Trygve Aaberge <trygve.aaberge@itera.no>
2015-03-11 23:46:03 +0100

    Remove unused imports and constants

--- src/no/itera/lego/Main.java
+++ src/no/itera/lego/Main.java
@@ -1,14 +1,8 @@
 package no.itera.lego;
 
 import lejos.hardware.Button;
-import lejos.hardware.motor.Motor;
-import lejos.hardware.port.*;
-import lejos.hardware.sensor.EV3IRSensor;
-import lejos.robotics.SampleProvider;
 
 public class Main {
-
-  public static final int SPEED = 300;
 
   public static void main(String[] args) {
     System.out.println("IteraBot reporting for duty");
