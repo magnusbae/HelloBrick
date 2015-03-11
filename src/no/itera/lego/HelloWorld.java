cee747b4a1f731773328e88e2ac98b4f33b46e30
Magnus Bae <magnus.bae@itera.no>
2015-03-11 18:08:50 +0100

    Working IR navigation

--- src/no/itera/lego/HelloWorld.java
+++ src/no/itera/lego/HelloWorld.java
@@ -6,7 +6,10 @@ import lejos.hardware.sensor.EV3IRSensor;
 import lejos.robotics.SampleProvider;
 
 public class HelloWorld {
-  public static void main (String[] args) {
+
+  public static final int SPEED = 720;
+
+  public static void main(String[] args) {
     System.out.println("LoopBot reporting for duty");
 
     long start = System.currentTimeMillis();
@@ -16,7 +19,7 @@ public class HelloWorld {
     SampleProvider rangeSampler = rangeSensor.getDistanceMode();
     float[] lastRange = new float[rangeSampler.sampleSize()];
 
-    int count = 0;
+    int count = 1;
     boolean run = true;
 
 
@@ -24,44 +27,51 @@ public class HelloWorld {
 
     System.out.println("Setup took " + (stop - start) + " ms");
 
-    Motor.A.setSpeed(720);
-    Motor.B.setSpeed(720);
+    Motor.A.setSpeed(SPEED);
+    Motor.B.setSpeed(SPEED);
     Motor.A.forward();
     Motor.B.forward();
 
-    while(run){
-      start = System.currentTimeMillis();
+    while (run) {
+      try {
+        Thread.sleep(20);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+//      start = System.currentTimeMillis();
       rangeSampler.fetchSample(lastRange, 0);
-      stop = System.currentTimeMillis();
+//      stop = System.currentTimeMillis();
 
       // System.out.println("Sampling took " + (stop - start) + " ms");
 
-      if(lastRange[0] < 40 && lastRange[0] > 5){
-        if(count % 3 != 0){
+      if (lastRange[0] <= 5) {
+        run = false;
+      } else if (lastRange[0]
+          < 40) {
+        if (count % 4 != 0) {
           count++;
-          Motor.A.setSpeed(0);
-        }else{
-          count = 0;
-          Motor.B.setSpeed(0);
+          Motor.A.stop();
+        } else {
+          count = 1;
+          Motor.B.stop();
         }
-        try{
+        try {
           Thread.sleep(500);
-        }catch (Exception e){
+        } catch (Exception e) {
           run = false;
           System.out.println("Stopping due to exception");
         }
-        Motor.A.setSpeed(720);
-        Motor.B.setSpeed(720);
-      }else if(lastRange[0] <= 5){
-        run = false;
+        Motor.A.forward();
+        Motor.B.forward();
       }
     }
 
-    Motor.A.stop();
+    Motor.A.stop(true);
     Motor.B.stop();
 
-    try{
+    try {
       Thread.sleep(1200);
-    }catch(Exception e){}
+    } catch (Exception e) {
+    }
   }
 }
