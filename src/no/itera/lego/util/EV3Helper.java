5aaead29f33c56980a1c17bb0379ae5bacd93924
Trygve Aaberge <trygve.aaberge@itera.no>
2016-03-30 20:41:49 +0200

    Remove obsolete code

--- src/no/itera/lego/util/EV3Helper.java
+++ src/no/itera/lego/util/EV3Helper.java
@@ -1,17 +1,9 @@
 package no.itera.lego.util;
 
-import java.lang.reflect.Field;
-
+import lejos.hardware.Sound;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.SensorPort;
-//import lejos.hardware.sensor.EV3IRSensor;
-import lejos.hardware.Sound;
-import lejos.robotics.Color;
 import lejos.robotics.RegulatedMotor;
-import lejos.robotics.SampleProvider;
-
-
 
 /**
  * Helper functions for basic operation of our pre-configured robot.
@@ -31,12 +23,6 @@ public class EV3Helper {
 
     private RegulatedMotor motorRight;
     private RegulatedMotor motorLeft;
-//    private EV3IRSensor irSensor;
-
-    private SampleProvider rangeSampler;
-    private float[] lastRange;
-
-    private String[] colors;
 
     private enum Direction {
         FORWARD, BACKWARD
@@ -53,13 +39,6 @@ public class EV3Helper {
         //Sets default motor speed for driving motors
         motorRight.setSpeed(DEFAULT_MOTOR_SPEED);
         motorLeft.setSpeed(DEFAULT_MOTOR_SPEED);
-
-//        irSensor = new EV3IRSensor(SensorPort.S2);
-
-//        rangeSampler = irSensor.getDistanceMode();
-//        lastRange = new float[rangeSampler.sampleSize()];
-
-        colors = getColors();
     }
 
     public RegulatedMotor getMotorRight() {
@@ -70,69 +49,12 @@ public class EV3Helper {
         return motorLeft;
     }
 
-
-//    public EV3IRSensor getIrSensor() {
-//        return irSensor;
-//    }
-
-//    public EV3ColorSensor getColorSensor() {
-//        return colorSensor;
-//    }
-
-
     /**
      * Plays a beep with the speaker.
      */
     public void playBeep() {
         Sound.beep();
     }
-
-    /**
-     * Fetches a distance sample from the EV3 infrared sensor.
-     * Close to centimeters.
-     * @return Distance from sensor in something that's quite close to centimeters
-     */
-    public float getDistance() {
-        rangeSampler.fetchSample(lastRange, 0);
-        return lastRange[0];
-    }
-
-    /**
-     * Fetches the color id from the EV3 color sensor.
-     * @return id of measured color
-     */
-//    public int getColorId() {
-//        return colorSensor.getColorID();
-//    }
-
-    /**
-     * Fetches a color sample from the EV3 color sensor.
-     * @return name of measured color
-     */
-////    public String getColorName() {
-//        int color = colorSensor.getColorID();
-//        if (color < 0 || color >= colors.length) {
-//            return "NONE";
-//        }
-//        return colors[color];
-//    }
-
-
-    /**
-     * Get a array of the colors in the Color class. Note that the EV3 does not
-     * support all of these colors. The colors it supports are: NONE, BLACK,
-     * BLUE, GREEN, YELLOW, RED, WHITE and BROWN.
-     * @return array of the colors
-     */
-    private String[] getColors() {
-        Field[] names = Color.class.getFields();
-        String[] list = new String[names.length];
-        for (int i = 0; i < list.length; i++) {
-            list[i] = names[i].getName();
-        }
-        return list;
-    }
-
 
     /**
      * Drives forward until stop is called.
