39dcba15d78f2f36a280b16ce9a11bf1ac1ccc7f
Sondre Lefsaker <sondrele@stud.ntnu.no>
2016-04-02 15:18:14 +0200

    refactor: intent file

--- src/no/itera/lego/message/Status.java
+++ src/no/itera/lego/message/Status.java
@@ -10,42 +10,50 @@ import java.util.List;
 
 public class Status implements Message {
 
-  public static final String TYPE = "status";
+    public static final String TYPE = "status";
 
-  // this is set to true when the server activates a match
-  public final boolean isActive;
-  // the current color of your robot
-  public final Color target;
-  // the current positions on the colored section of the map of all robots
-  // 0: you
-  // 1: your teammate
-  // 2: opponent one
-  // 3: opponent two
-  public final List<Color> colors;
+    // this is set to true when the server activates a match
+    public final boolean isActive;
+    // the current color of your robot
+    public final Color target;
+    // the current positions on the colored section of the map of all robots
+    // 0: you
+    // 1: your teammate
+    // 2: opponent one
+    // 3: opponent two
+    public final List<Color> colors;
 
-  public Status(boolean isActive, Color target, List<Color> colors) {
-    this.isActive = isActive;
-    this.target = target;
-    this.colors = colors;
-  }
-
-  public static Status fromJson(JSONObject object) {
-    boolean isActive = (boolean) object.get("isActive");
-
-    Color target = Color.valueOf((String) object.get("target"));
-
-    JSONArray jsonColorsArray = (JSONArray)object.get("status");
-
-    List<Color> colors = new ArrayList<Color>();
-    for (Object objColor : jsonColorsArray) {
-      colors.add(Color.valueOf((String) objColor));
+    public Status(boolean isActive, Color target, List<Color> colors) {
+        this.isActive = isActive;
+        this.target = target;
+        this.colors = colors;
     }
 
-    return new Status(isActive, target, colors);
-  }
+    public static Status fromJson(JSONObject object) {
+        boolean isActive = (boolean) object.get("isActive");
 
-  public String toJson() {
-    throw new UnsupportedOperationException(String.format("Cannot convert message type '%s' to JSON", TYPE));
-  }
+        Color target = Color.valueOf((String) object.get("target"));
+
+        JSONArray jsonColorsArray = (JSONArray)object.get("status");
+
+        List<Color> colors = new ArrayList<Color>();
+        for (Object objColor : jsonColorsArray) {
+            colors.add(Color.valueOf((String) objColor));
+        }
+        return new Status(isActive, target, colors);
+    }
+
+    public String toJson() {
+        throw new UnsupportedOperationException(String.format("Cannot convert message type '%s' to JSON", TYPE));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s{%s, %s, %s}",
+                getClass().getSimpleName(),
+                isActive,
+                target,
+                colors);
+    }
 
 }
