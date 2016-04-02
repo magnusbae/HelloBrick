bd4656ccd293dbccc1bcf7e1938a425755bafaeb
Sondre Lefsaker <sondrele@stud.ntnu.no>
2016-04-02 15:03:02 +0200

    doc: add some explaining comments to the RobotState member fields

--- src/no/itera/lego/util/RobotState.java
+++ src/no/itera/lego/util/RobotState.java
@@ -7,19 +7,46 @@ import no.itera.lego.message.Status;
 
 public class RobotState {
 
-    public static final String HOST = "10.0.1.3";
+    // enter the server address:port that the robot should connect to
+    public static final String HOST = "192.168.43.168";
     public static final int PORT = 3004;
+    // the server will set this to true when your round starts
     public boolean shouldRun;
-    public CountDownLatch latch;
+    // this is the last status received by the server
     public Status lastStatus;
+    // this is your robot's last read color, i.e. your position on the map
+    // it is set automatically from the sensor thread
+    // note that this color will be slightly more  up to date than your
+    // color in lastStatus (lastStatus.color.get(0)), although not by much
     public Color lastColor = Color.UNDEFINED;
+    // this is your robot's last read distance
     public float lastDistance;
+    // you don't need to worry about these fields
+    public CountDownLatch latch;
     public boolean webSocketOpen;
     public boolean webSocketConnecting;
+    // you use the robotController to control your robot
+    // it contains a few helper functions that let you control the robot with
+    // _non blocking_ function calls, which means that the robot will keep
+    // driving in the specified direction until you tell it to do something else
     public final RobotController robotController;
 
     public RobotState(RobotController robotController) {
         this.robotController = robotController;
         shouldRun = true;
     }
+
+    @Override
+    public String toString() {
+        return String.format("%s{%s, %s, %s, %s, %s, %s, %s}",
+                getClass().getSimpleName(),
+                shouldRun,
+                lastStatus,
+                lastColor,
+                lastDistance,
+                webSocketOpen,
+                webSocketConnecting,
+                robotController);
+    }
+
 }
